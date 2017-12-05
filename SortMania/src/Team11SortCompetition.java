a5381df12d19831fc7353f27eeb03670d90b9979
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-12-05 12:08:51 -0500

    12/5/17

--- SortMania/src/Team11SortCompetition.java
+++ SortMania/src/Team11SortCompetition.java
@@ -3,11 +3,39 @@ public class Team11SortCompetition {
 	public static void main(String[] args)
 	{
 		int[] testList = {5, 1, 6, 2, 4, 3};
-		System.out.println(standardSort(testList));
+		System.out.println(challengeOne(testList));
 		for(int i = 0; i < testList.length; i++)
 		{
 			System.out.println(testList[i]);
 		}
+	}
+	
+	public static int challengeOne(int[] list1)
+	{
+		selectionSort(list1);
+		return (list1[list1.length/2 - 1] + list1[list1.length/2])/2;
+	}
+	
+	public static int challengeTwo(String[] list1, String find)
+	{
+		bubbleSort(list1);
+		for (int i = 0; i < list1.length; i++)
+		{
+			if(list1[i].compareTo(find) == 0)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public static int challengeFour(int[][] list1)
+	{
+		int[] medians = new int[1000];
+
+		
+		return challengeOne(medians);
+		
 	}
 	
 	public static int[] selectionSort(int[] list1)
@@ -68,25 +96,4 @@ public class Team11SortCompetition {
 			
 		}
 	}
-	
-	public static int challengeOne(int[] list1)
-	{
-		selectionSort(list1);
-		return (list1[4999] + list1[5000])/2;
-	}
-	
-	public static int challengeTwo(String[] list1, String find)
-	{
-		bubbleSort(list1);
-		for (int i = 0; i < list1.length; i++)
-		{
-			if(list1[i].compareTo(find) == 0)
-			{
-				return i;
-			}
-		}
-		return -1;
-	}
-	
-	public static int challengeThree()
 }
